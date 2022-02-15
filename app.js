a29db28a5787570645a069f9c304fda46e768eb1
Heo-JunHyeok <junhyeok_1996@naver.com>
2022-02-15 17:05:23 +0900

    ~#2.8

---
+++ app.js
@@ -0,0 +1,9 @@
+const player = {
+  name: "kim",
+  sayHello: function (otherPersonName) {
+    console.log("hello " + otherPersonName + " nice to meet you");
+  },
+};
+
+player.sayHello("lynn");
+player.sayHello("nico");
