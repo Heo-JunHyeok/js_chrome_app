9b661a0df096141bf76a6b306eda28b76bffdea0
Heo-JunHyeok <junhyeok_1996@naver.com>
2022-02-18 02:35:05 +0900

    ~#3.5

--- app.js
+++ app.js
@@ -1,9 +1,38 @@
-const age = parseInt(prompt("How old are you?"));
+const h1 = document.querySelector(".hello h1");
 
-if (isNaN(age) || age < 0) {
-  console.log("Please write a number");
-} else if (age < 18) {
-  console.log("You are too young.");
-} else {
-  console.log("You can drink");
+function handleTitleClick() {
+  h1.style.color = "blue";
 }
+
+function handleMouseEnter() {
+  h1.innerHTML = "Mouse is here";
+}
+
+function handleMouseLeave() {
+  h1.innerHTML = "Mouse is gone";
+}
+
+function handleWindowResize() {
+  document.body.style.backgroundColor = "tomato";
+}
+
+function handleWindowCopy() {
+  alert("copier");
+}
+
+function handleWindowOffline() {
+  alert("SOS no WIFI");
+}
+
+function handleWindowOnline() {
+  alert("All Good");
+}
+
+h1.onclick = handleTitleClick;
+h1.addEventListener("mouseenter", handleMouseEnter);
+h1.addEventListener("mouseleave", handleMouseLeave);
+
+window.addEventListener("resize", handleWindowResize);
+window.addEventListener("copy", handleWindowCopy);
+window.addEventListener("offline", handleWindowOffline);
+window.addEventListener("online", handleWindowOnline);
