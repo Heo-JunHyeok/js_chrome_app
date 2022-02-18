84975261235220482e2a7faff4ac0f013bb14550
Heo-JunHyeok <junhyeok_1996@naver.com>
2022-02-19 01:55:26 +0900

    #3 finish

--- app.js
+++ app.js
@@ -1,38 +1,7 @@
 const h1 = document.querySelector(".hello h1");
 
 function handleTitleClick() {
-  h1.style.color = "blue";
+  h1.classList.toggle("clicked"); //class에 없으면 추가, 있으면 제거
 }
 
-function handleMouseEnter() {
-  h1.innerHTML = "Mouse is here";
-}
-
-function handleMouseLeave() {
-  h1.innerHTML = "Mouse is gone";
-}
-
-function handleWindowResize() {
-  document.body.style.backgroundColor = "tomato";
-}
-
-function handleWindowCopy() {
-  alert("copier");
-}
-
-function handleWindowOffline() {
-  alert("SOS no WIFI");
-}
-
-function handleWindowOnline() {
-  alert("All Good");
-}
-
-h1.onclick = handleTitleClick;
-h1.addEventListener("mouseenter", handleMouseEnter);
-h1.addEventListener("mouseleave", handleMouseLeave);
-
-window.addEventListener("resize", handleWindowResize);
-window.addEventListener("copy", handleWindowCopy);
-window.addEventListener("offline", handleWindowOffline);
-window.addEventListener("online", handleWindowOnline);
+h1.addEventListener("click", handleTitleClick);
